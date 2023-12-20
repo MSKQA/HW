f3809c33ef2acf9d35e990b57ae9d4067f2ba446
MSKQA <meher.kuralla@gmail.com>
2023-12-20 20:07:37 +0530

    I am pushing Lab003

---
+++ src/Dec17/Lab003.java
@@ -0,0 +1,20 @@
+package Dec17;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        int age_meher=38;
+        int age_ravi=35;
+        boolean result= age_ravi<age_meher;
+        System.out.println(result);
+
+        System.out.println(10==10);
+        System.out.println(10>=10);
+        System.out.println(10<=10);
+        System.out.println(10> 10);
+        System.out.println(10<10);
+
+        byte b=65;
+        System.out.println('A'==b);
+
+    }
+}
