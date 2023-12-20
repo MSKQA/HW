358b509c1765a652f856e22976750a72b2653b9a
MSKQA <meher.kuralla@gmail.com>
2023-12-20 18:00:38 +0530

    I am pushing Lab002

---
+++ src/Dec17/Lab002.java
@@ -0,0 +1,18 @@
+package Dec17;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        System.out.println(a+b);
+        String name="Meher";
+        String name1="Kuralla";
+        System.out.println(a+b+name+name1);
+        System.out.println(name+a+b+name1);
+
+        int c=15;
+        //compound assignment operator
+        c +=26;
+        System.out.println(c);
+    }
+}
